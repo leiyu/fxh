152e90323c478ea96bc88e2518aefb1fd0691b3a
Yu Lei <t@leiyu.net>
2017-09-04 23:23:37 +0800

    flask+bootstrape table+echarts

--- dao.py
+++ dao.py
@@ -1,10 +1,23 @@
 import json
 import sqlite3
+import flask
 
 
 db_file = r'fxh.db'
 
+"""
+表结构
+ (id int, dataID int, forumID int, webName text, forumName text,
+     topicID text, topicTitle text,	topicLink text, author text,
+     pubTime text, authorLink text, replyNum int, viewNum int,
+     lastReplier text, lastReplyTime text, lastReplierLink text, 
+     topicContent text, spiderDate text, spiderTime text, 
+     SYSspiderTime text, mediaType text, Dlevel int, isManu int,
+     DTopicKind int, effectIndex int, DlevelKey text,	alexa int,
+     remark text,	xid int)''') 
 
+"""
+#媒体类型
 def CountByMediaType():
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
@@ -12,4 +25,76 @@ def CountByMediaType():
     rs = c.fetchall()
     conn.close()
     return(json.dumps(rs))
+
+CountByMediaType()
+
+#媒体名称
+def CountByWebName():
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+    c.execute('SELECT webName, count(ID) as num FROM articles group by webName order by count(ID) desc limit 30')
+    rs = c.fetchall()
+    conn.close()
+    return(json.dumps(rs))
+
+#时间序列
+def CountByDate():
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+    c.execute('SELECT date(pubTime), count(ID) as num FROM articles group by date(pubTime) order by date(pubTime) ')
+    rs = c.fetchall()
+    conn.close()
+    return(json.dumps(rs))
+
+CountByDate()
+
+
+
+def getArticles(currPage=0, pageSize=10):
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
     
+    #获得所有记录总数
+    
+    c.execute('SELECT count(*) FROM articles')
+    rs = c.fetchone()
+    
+    rowsCount = rs[0]        
+    #print(rowsCount)    
+    
+    c.execute('SELECT ID,mediaType,webName,topicTitle,author,topicLink,pubTime,DtopicKind,viewNum,replyNum FROM articles limit ' +  str(pageSize) + ' offset ' + str((currPage -1)*pageSize))
+    rs = c.fetchall()
+    
+    
+    data = []
+    for r in rs:  
+        d = {}
+        d['id'] = r[0]
+        d['mediaType'] = r[1]
+        d['webName'] = r[2]
+        d['topicTitle'] = r[3]
+        d['author'] = r[4]        
+        d['topicLink'] = r[5]
+        d['pubTime'] = r[6]
+        d['DtopicKind'] = r[7]
+        d['viewNum'] = r[8]
+        d['replyNum'] = r[9]
+        
+        
+        data.append(d)
+    conn.close()
+
+    #去掉前后的 []
+    jsonStr = json.dumps(data)
+    
+    jsonStr = '{"total":' + str(rowsCount) + ',"rows":' + jsonStr + '}';  
+    
+    return jsonStr
+    #return jsonStr[1:-1]
+        
+    #return rs
+    #return(json.dumps(rs))
+
+
+getArticles(1,2)
+
--- dataAPI.py
+++ dataAPI.py
@@ -6,10 +6,21 @@ from flask import Flask
 from flask import request
 from flask import redirect
 from flask import jsonify
+from flask import Response
+from flask import send_file
+
 
 import dao
 
-app = Flask(__name__)
+#app = Flask(__name__)
+
+app = Flask(__name__,static_url_path='',root_path=r'D:\wwwroot\flask\fxh')  
+
+def Response_headers(content):
+    resp = Response(content)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    return resp
+
 
 @app.route('/' , methods=['GET', 'POST'])
 def index():
@@ -35,13 +46,53 @@ def login():
 #获得不同mediaType的统计
 @app.route('/getCountByMediaType', methods=['POST', 'GET'])
 def getCountByMediaType():
-        
-    return dao.CountByMediaType()
+    content = dao.CountByMediaType()
+    resp = Response_headers(content)
+    return resp
     #if request.method == 'GET':
     #    return 'ciao ciao:' + request.form['username']
     
+#获得不同webName的统计
+@app.route('/getCountByWebName', methods=['POST', 'GET'])
+def getCountBywebName():    
+    content = dao.CountByWebName()
+    resp = Response_headers(content)
+    return resp
+
+#获得不同时间的统计
+@app.route('/getArticleByDate', methods=['POST', 'GET'])
+def getCountByDate():    
+    content = dao.CountByDate()
+    resp = Response_headers(content)
+    return resp
+
+
+#获得不同mediaType的统计
+@app.route('/getArticles', methods=['POST', 'GET'])
+def getArticles():
+    currPage = int(request.args.get('pageNumber', '0'))
+    pageSize = int(request.args.get('pageSize', '10'))
+    content = dao.getArticles(currPage,pageSize)
+    resp = Response_headers(content)
+    return resp
+
+#测试request
+@app.route('/testRequest')
+def test_request():
+    return 'currPage=' + request.args.get('pageNumber', '') + ',pageSize=' + request.args.get('pageSize', '')
+
     
+#静态文件
+@app.route('/test')
+def test_html():
+    return send_file("bst_test.html")
     
+#静态文件
+@app.route('/report')
+def test_report():
+    return send_file("report.html")
+
+
     
     
 if __name__ =='__main__':
